5a8b6753c8691055ad831232adef7f984dd98f48
limzykenneth <limzy.kenneth@gmail.com>
2017-03-20 18:29:57 +0800

    More steps towards creating new collection

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const session = require("express-session");
@@ -6,6 +7,7 @@ const bcrypt = require("bcrypt");
 const path = require("path");
 const f = require("util").format;
 const MongoStore = require('connect-mongo')(session);
+const MongoClient = require('mongodb').MongoClient;
 
 // Custom middleware
 var restrict = require("./restrict.js");
@@ -36,15 +38,6 @@ router.use(function(req, res, next){
 	next();
 });
 
-router.get("/", restrict, function(req, res) {
-    res.render("index", {title: "Express"});
-});
-
-// User the "restrict" middleware to handle routes not meant for unauthorised access
-router.get("/restricted", restrict, function(req, res){
-	res.send("Wahoo! restricted area, click to <a href=\"/admin/logout\">logout</a>");
-});
-
 // Logout by destroying the session
 router.get("/logout", function(req, res){
 	req.session.destroy(function(){
@@ -67,7 +60,7 @@ router.post("/login", function(req, res){
 				// or in this case the entire user object
 				req.session.user = user;
 				req.session.success = "Authenticated as " + user.name + " click to <a href=\"/admin/logout\">logout</a>. You may now access <a href=\"/restricted\">/restricted</a>.";
-				res.redirect(path.join(currentPath + "/restricted"));
+				res.redirect(path.join(currentPath + "/"));
 			});
 		} else {
 			req.session.error = "Authentication failed, please check your username and password. (use \"tj\" and \"foobar\")";
@@ -75,5 +68,74 @@ router.post("/login", function(req, res){
 	    }
 	});
 });
+
+// User the "restrict" middleware to handle routes not meant for unauthorised access
+router.use(restrict, function(req, res, next){
+	next();
+});
+
+router.get("/", function(req, res) {
+    res.render("index", {title: "Express"});
+});
+
+router.get("/collections", function(req, res){
+	res.render("collections");
+});
+
+router.post("/schema/new", function(req, res){
+	let data = req.body;
+	if(!validateIncoming(data.collectionName)){
+		res.json({
+			status: "failed",
+			reason: "Collection names should only contain alphanumeric characters, underscore and spaces."
+		});
+		return;
+	}
+	data.collectionSlug = data.collectionName.toLowerCase();
+
+	_.each(data.fields, function(el, i){
+		if(!validateIncoming(el.name)){
+			res.json({
+				status: "failed",
+				reason: "Field names should only contain alphanumeric characters, underscore and spaces."
+			});
+			return false;
+		}
+		el.slug = el.name.toLowerCase().replace(" ", "_");
+	});
+
+	MongoClient.connect(mongoURL, function(err, db){
+		if(err) throw err;
+
+		db.collection("_schema").find({collectionSlug: data.collectionSlug}).toArray(function(err, result){
+			if(err) throw err;
+			if(result.length > 0){
+				res.json({
+					status: "failed",
+					reason: "Collection with that name already exist."
+				});
+				db.close();
+			}else{
+				db.collection("_schema").insertOne(data, function(err){
+					if(err) throw err;
+
+					res.json({
+						status: "success"
+					});
+					db.close();
+				});
+			}
+		});
+	});
+});
+
+function validateIncoming(string){
+	let regexp = /^[a-zA-Z0-9-_ ]+$/;
+	if (string.search(regexp) == -1){
+		return false;
+	}else{
+		return true;
+	}
+}
 
 module.exports = router;
---
+++ static/javascripts/src/custom.js
@@ -0,0 +1,95 @@
+var $ = require("jquery");
+var _ = require("lodash/core");
+_.template = require("lodash/template");
+// var _ = require("underscore");
+
+$(document).ready(function() {
+	$("#page-content").on("click", ".schema-definition .schema-add", function(e) {
+		var renderField = _.template($("#schema-creation-template").html());
+
+		$(this).parents(".field").after(renderField());
+		$("#page-content .schema-definition .field").each(function(i) {
+			$(this).find(".name-field").attr("name", "name-" + i);
+			$(this).find(".type-field").attr("name", "type-" + i);
+		});
+	});
+	$("#page-content").on("click", ".schema-definition .schema-remove", function(e) {
+		$(this).parents(".field").remove();
+	});
+
+	$("#page-content").on("change", ".schema-definition .field .type-field", function(){
+		$(this).parent().siblings(".choices").remove();
+		if($(this).val() == "checkbox" || $(this).val() == "radio"){
+			$(this).parents(".field").append($("#checkbox-options-template").html());
+		}
+	});
+
+
+	$("#page-content .schema-definition").submit(function(e) {
+		e.preventDefault();
+
+		var rawData = $(this).serializeArray();
+		var req = {
+			collectionName: rawData[0].value,
+			fields: []
+		};
+
+		var choices = [];
+		$(this).find("textarea").each(function(i){
+			choices.push($(this).val());
+		});
+		var choicesIndex = 0;
+		for(var i = 1; i < rawData.length; i = i + 2){
+			var buffer = {};
+			buffer.name = rawData[i].value;
+			buffer.type = rawData[i+1].value;
+			if(buffer.type == "checkbox" || buffer.type == "radio"){
+				buffer.properties = {};
+				var choiceList = choices[choicesIndex].split("\n");
+				var choiceObject = {};
+
+				// Extras to allow custom definitions of options values
+				_.each(choiceList, function(el, i){
+					var reg = [/^"(.+?)":"(.+?)"$/g, /^"(.+?)":(.+?)$/g, /^(.+?):"(.+?)"$/g, /^(.+?):(.+?)$/g];
+					if(reg[0].test(el)){
+						choiceObject[el.replace(reg[0], "$1")] = el.replace(reg[0], "$2");
+					}else if(reg[1].test(el)){
+						choiceObject[el.replace(reg[1], "$1")] = el.replace(reg[1], "$2");
+					}else if(reg[2].test(el)){
+						choiceObject[el.replace(reg[2], "$1")] = el.replace(reg[2], "$2");
+					}else if(reg[3].test(el)){
+						choiceObject[el.replace(reg[3], "$1")] = el.replace(reg[3], "$2");
+					}else{
+						choiceObject[el] = el;
+					}
+				});
+
+				buffer.properties.choices = choiceObject;
+				choicesIndex++;
+			}
+			req.fields.push(buffer);
+		}
+
+		var $submitBtn = $(this).find(".submit input");
+		$submitBtn.attr("value", "Saving...").prop("disabled", true);
+		fetch("/admin/schema/new", {
+			method: "post",
+			body: JSON.stringify(req),
+			credentials: "include",
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+		    },
+		}).then(function(res){
+			return res.json();
+		}).then(function(data){
+			if(data.status == "success"){
+				// redirect somewhere else
+				console.log("redirect");
+			}else{
+				$("#page-content .collection-creation .error-msg").text(data.reason).show().delay(2000).fadeOut(500);
+				$submitBtn.attr("value", "Save").prop("disabled", false);
+			}
+		});
+	});
+});
