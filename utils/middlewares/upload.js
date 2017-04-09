5c6fc5ad8ae266ded270aac7da481d1e93d948d3
limzykenneth <limzy.kenneth@gmail.com>
2017-04-09 12:42:19 +0800

    Fix bug where no fields are recorded when no image upload field present

--- utils/middlewares/upload.js
+++ utils/middlewares/upload.js
@@ -25,51 +25,25 @@ const uploadImage = multer({
 		fileSize: 20000000
 	}
 });
-const uploadFile = multer({
-	storage: multer.diskStorage({
-		destination: function (req, file, cb) {
-			cb(null, "./uploads/files");
-		},
-		filename: function (req, file, cb) {
-			cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + randomstring.generate(10) + path.extname(file.originalname));
-		}
-	}),
-	limits: {
-		fileSize: 10000
-	}
-});
 
-var uploadSchemas = generator(uploadImage);
+var uploadSchemas = function(req, res, next){
+	connect.then(function(db){
+		db.collection("_schema").findOne({collectionSlug: req.params.collection}, function(err, result){
+			if(err) throw err;
 
-function generator(){
-	var multerSettings = Array.prototype.slice.call(arguments);
-	var newFunctions = [];
-
-	_.each(multerSettings, function(setting){
-		var newFunction = function(req, res, next){
-			connect.then(function(db){
-				db.collection("_schema").findOne({collectionSlug: req.params.collection}, function(err, result){
-					if(err) throw err;
-
-					var fields = [];
-					_.each(result.fields, function(el, i){
-						if(el.type == "image"){
-							fields.push({name: el.slug, maxCount: 1});
-						}
-					});
-					if(fields.length !== 0){
-						setting.fields(fields)(req, res, next);
-					}else{
-						next();
-					}
-				});
+			var fields = [];
+			_.each(result.fields, function(el, i){
+				if(el.type == "image"){
+					fields.push({name: el.slug, maxCount: 1});
+				}
 			});
-		};
-
-		newFunctions.push(newFunction);
+			if(fields.length !== 0){
+				uploadImage.fields(fields)(req, res, next);
+			}else{
+				uploadImage.none()(req, res, next);
+			}
+		});
 	});
-
-	return newFunctions;
-}
+};
 
 module.exports = uploadSchemas;
