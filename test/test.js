c275f56b89ca05dab065fdc72b44a9f50a0242cd
limzykenneth <limzy.kenneth@gmail.com>
2017-04-13 23:31:21 +0800

    Modularize tests

--- test/test.js
+++ test/test.js
@@ -174,31 +174,3 @@ describe("While unauthorised", function(){
 		});
 	});
 });
-
-describe("While signed in as admin", function(){
-	request(app)
-	.post("/admin/login")
-	.type("form")
-	.send({
-		"username": "admin",
-		"password": "admin"
-	})
-	.expect(302)
-	.end(function(err, res){
-		describe("GET /admin/collection", function(){
-			it("should return with status code 200");
-		});
-
-		describe("GET /admin/users", function(){
-			it("should return with status code 200");
-		});
-
-		describe("GET /admin/account", function(){
-			it("should return with status code 200");
-		});
-
-		describe("GET /admin/config", function(){
-			it("should return with status code 200");
-		});
-	});
-});
