0586f8fbde85226c097c5eaee874a85171cd19a6
limzykenneth <limzy.kenneth@gmail.com>
2017-04-16 20:00:08 +0800

    Added supertest-session

--- test/whileAdmin.js
+++ test/whileAdmin.js
@@ -1,32 +1,55 @@
 const request = require("supertest");
+const session = require("supertest-session");
 const assert = require("chai").assert;
 const app = require("../app.js");
 const connect = require("../utils/database.js");
 
+var testSession = null;
+
+beforeEach(function(){
+	testSession = session(app);
+});
+
 describe("While signed in as admin", function(){
-	request(app)
-	.post("/admin/login")
-	.type("form")
-	.send({
-		"username": "admin",
-		"password": "admin"
-	})
-	.expect(302)
-	.end(function(err, res){
-		describe("GET /admin/collection", function(){
-			it("should return with status code 200");
-		});
+	var authenticatedSession;
 
-		describe("GET /admin/users", function(){
-			it("should return with status code 200");
-		});
+	beforeEach(function(done){
+		testSession.post("/admin/login")
+			.send({username: "admin", password: "admin"})
+			.expect(302)
+			.end(function(err){
+				if(err) throw err;
+				authenticatedSession = testSession;
+				return done();
+			});
+	});
 
-		describe("GET /admin/account", function(){
-			it("should return with status code 200");
-		});
+	var expectOKResponse = function(url, done){
+		authenticatedSession.get(url)
+			.expect(200, done);
+	};
 
-		describe("GET /admin/config", function(){
-			it("should return with status code 200");
+	describe("GET /admin/collections", function(){
+		it("should return with status code 200", function(done){
+			expectOKResponse("/admin/collections", done);
+		});
+	});
+
+	describe("GET /admin/users", function(){
+		it("should return with status code 200", function(done){
+			expectOKResponse("/admin/users", done);
+		});
+	});
+
+	describe("GET /admin/account", function(){
+		it("should return with status code 200", function(done){
+			expectOKResponse("/admin/account", done);
+		});
+	});
+
+	describe("GET /admin/config", function(){
+		it("should return with status code 200", function(done){
+			expectOKResponse("/admin/config", done);
 		});
 	});
 });
