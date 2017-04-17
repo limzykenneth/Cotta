cfd63f10fa0a886fda93dd1d83abb5f6434135a7
limzykenneth <limzy.kenneth@gmail.com>
2017-04-17 19:15:42 +0800

    Tearing down more pyramids

--- routes/admin/collections.js
+++ routes/admin/collections.js
@@ -5,8 +5,10 @@ const path = require("path");
 const connect = require("../../utils/database.js");
 const uploadSchemas = require("../../utils/middlewares/upload.js");
 const restricted = require("../../utils/middlewares/restrict.js");
-const autoIncrement = require("mongodb-autoincrement");
 const moment = require("moment");
+const Promise = require("bluebird");
+const autoIncrement = require("mongodb-autoincrement");
+Promise.promisifyAll(autoIncrement);
 
 // Setting locals
 router.use(function(req, res, next){
@@ -27,13 +29,9 @@ router.get("/new", restricted.toEditor, function(req, res){
 });
 
 // Render page to edit existing collection
-router.get("/edit/:collection", restricted.toEditor, function(req, res){
-	connect.then(function(db){
-		db.collection("_schema").findOne({collectionSlug: req.params.collection}, function(err, result){
-			result.schemas = res.locals.schemas;
-			res.render("edit-collection", result);
-		});
-	});
+router.get("/edit/:collection", restricted.toEditor, function(req, res, next){
+	var schema = _.find(res.locals.schemas, {collectionSlug: req.params.collection});
+	res.render("edit-collection", schema);
 });
 
 // Models ----------------------------------------------------------------------------
@@ -47,43 +45,42 @@ router.use(function(req, res, next){
 router.get("/:collection", function(req, res, next){
 	connect.then(function(db){
 		// Get schema of the specified collection
-		db.collection("_schema").findOne({collectionSlug: req.params.collection}, function(err, schema){
-			if(err) throw err;
+		var schema = _.find(res.locals.schemas, {collectionSlug: req.params.collection});
 
-			// No need to continue if schema doesn't exist
-			if(typeof schema == "undefined") {
-				next();
-			}else{
-				// Get models of the specified collection
-				db.collection(schema.collectionSlug).find().toArray(function(err, models){
-					if(err) throw err;
-
-					res.render("models", {
-						collectionName: schema.collectionName,
-						collectionSlug: schema.collectionSlug,
-						schema: schema,
-						data: models
-					});
+		// No need to continue if schema doesn't exist
+		if(typeof schema == "undefined") {
+			next();
+		}else{
+			// Get models of the specified collection
+			db.collection(schema.collectionSlug).find().toArray().then(function(models){
+				res.render("models", {
+					collectionName: schema.collectionName,
+					collectionSlug: schema.collectionSlug,
+					schema: schema,
+					data: models
 				});
-			}
-		});
+			});
+		}
 	}).catch(function(err){
-		concole.log(err);
+		next(err);
 	});
 });
 
 // Render page to create new model under specified collection
 router.get("/:collection/new", function(req, res){
 	connect.then(function(db){
-		db.collection("_schema").findOne({collectionSlug: req.params.collection}, function(err, result){
+		db.collection("_schema").findOne({collectionSlug: req.params.collection}).then(function(result){
 			res.render("create-model", result);
 		});
+	}).catch(function(err){
+		next(err);
 	});
 });
 
 // Create new model under specified collection
 router.post("/:collection/new", uploadSchemas, function(req, res){
 	var data = req.body;
+	var schema = _.find(res.locals.schemas, {collectionSlug: req.params.collection});
 
 	// Save the uploaded file's path as a field value
 	_.each(req.files, function(el, i){
@@ -97,41 +94,49 @@ router.post("/:collection/new", uploadSchemas, function(req, res){
 		date_modified: moment.utc().format()
 	};
 
+	_.each(schema.fields, function(field){
+		if(field.type == "checkbox" && !Array.isArray(data[field.slug])){
+			let buffer = data[field.slug];
+			data[field.slug] = [buffer];
+		}
+	});
+
 	connect.then(function(db){
 		// Set increment index (should be abstracted if RDBS is to be used)
 		autoIncrement.setDefaults({collection: "_counters"});
-		autoIncrement.getNextSequence(db, req.params.collection, function(err, autoIndex){
-			if(err) throw err;
-
+		return autoIncrement.getNextSequenceAsync(db, req.params.collection).then(function(autoIndex){
 			// Set unique auto incrementing index
 			data._uid = autoIndex;
-
-			// Create index and ensure it is unique in database
-			db.collection(req.params.collection).createIndex({"_uid": 1}, {unique: true}, function(err){
-				if(err) throw err;
-
-				// Insert data into database under specified collection
-				db.collection(req.params.collection).insertOne(data, function(err){
-					if(err) throw err;
-
-					// Add model info to the user who submitted it
-					db.collection("_users_auth").updateOne({username: req.session.user.username},
-						{$addToSet:{models: `${req.params.collection}.${data._uid}`}},
-						function(err){
-						if(err) throw err;
-
-						res.json({
-							status: "success"
-						});
-					});
-				});
-			});
+			return Promise.resolve(db);
 		});
+	}).then(function(db){
+		// Create index and ensure it is unique in database
+		return db.collection(req.params.collection).createIndex({"_uid": 1}, {unique: true}).then(function(){
+			return Promise.resolve(db);
+		});
+	}).then(function(db){
+		// Insert data into database under specified collection
+		Promise.join(
+			db.collection(req.params.collection).insertOne(data),
+			db.collection("_users_auth").updateOne({username: req.session.user.username},
+				{
+					$addToSet:{
+						models: `${req.params.collection}.${data._uid}`
+					}
+				}),
+			function(){
+				res.json({
+					status: "success"
+				});
+			}
+		);
+	}).catch(function(err){
+		next(err);
 	});
 });
 
 // Prepare data for model rendering
-router.get("/:collection/:id/?*", function(req, res, next){
+router.get(["/:collection/:id", "/:collection/:id/edit"], function(req, res, next){
 	connect.getSchemaModel(req.params.collection, req.params.id).then(function(r){
 		var data = r.schema;
 		data._uid = r.model._uid;
@@ -145,7 +150,7 @@ router.get("/:collection/:id/?*", function(req, res, next){
 		req.modelData = data;
 		next();
 	}).catch(function(err){
-		throw err;
+		next(err);
 	});
 });
 
@@ -167,6 +172,7 @@ router.post("/:collection/:id/edit", uploadSchemas, function(req, res){
 			if(err) throw err;
 
 			var data = req.body;
+			var schema = _.find(res.locals.schemas, {collectionSlug: req.params.collection});
 
 			// Save the uploaded file's path as a field value
 			_.each(req.files, function(el, i){
@@ -181,6 +187,13 @@ router.post("/:collection/:id/edit", uploadSchemas, function(req, res){
 				date_created: model._metadata.date_created,
 				date_modified: moment.utc().format()
 			};
+
+			_.each(schema.fields, function(field){
+				if(field.type == "checkbox" && !Array.isArray(data[field.slug])){
+					let buffer = data[field.slug];
+					data[field.slug] = [buffer];
+				}
+			});
 
 			// Probably don't want to use $set (maybe dunno)
 			db.collection(req.params.collection).updateOne({"_uid": parseInt(req.params.id)}, {$set: data}, function(err){
