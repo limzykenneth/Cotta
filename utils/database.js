0b229ddbe32ba4be5ce195ce797ad8e4d327f699
limzykenneth <limzy.kenneth@gmail.com>
2017-04-17 21:49:29 +0800

    Power hammer

--- utils/database.js
+++ utils/database.js
@@ -5,38 +5,24 @@ const MongoClient = require('mongodb').MongoClient;
 let mongoURL = f("mongodb://%s:%s@%s/%s", process.env.mongo_user, process.env.mongo_pass, process.env.mongo_server, process.env.mongo_db_name);
 
 // Utilize Promises to share the same connection for the whole app
-let connection = new Promise(function(resolve, reject){
-	MongoClient.connect(mongoURL, {poolSize: 10}, function(err, db){
-		if(err){
-			reject(err);
-		}else{
-			resolve(db);
-		}
-	});
-});
+let connection = MongoClient.connect(mongoURL, {poolSize: 10});
 
-// Callback hell here for convenience so that schema and model can be returned at once
+// For convenience so that schema and model can be returned at once
 connection.getSchemaModel = function(collectionSlug, modelID){
-	return new Promise(function(resolve, reject){
-		connection.then(function(db){
-			db.collection("_schema").findOne({collectionSlug: collectionSlug}, function(err, schema){
-				if (err) {
-					reject(err);
-				}else{
-					db.collection(collectionSlug).findOne({"_uid": parseInt(modelID)}, function(err, model){
-						if(err){
-							reject(err);
-						}else{
-							resolve({
-								schema: schema,
-								model: model,
-								db: db
-							});
-						}
-					});
-				}
-			});
+	var returnObject = {};
+	return connection.then(function(db){
+		returnObject.db = db;
+		return db.collection("_schema").findOne({collectionSlug: collectionSlug}).then(function(schema){
+			returnObject.schema = schema;
+			return Promise.resolve(db);
 		});
+	}).then(function(db){
+		return db.collection(collectionSlug).findOne({"_uid": parseInt(modelID)}).then(function(model){
+			returnObject.model = model;
+			return Promise.resolve();
+		});
+	}).then(function(){
+		return Promise.resolve(returnObject);
 	});
 };
 
