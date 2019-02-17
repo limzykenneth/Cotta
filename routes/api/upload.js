0d0b0149c2ede6994519825fb37917b2444845fe
limzykenneth <limzy.kenneth@gmail.com>
2019-02-17 15:56:37 +0000

    URL upload refactor 1

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,6 +1,5 @@
 // Main entry point for API uploading routes
 require("dotenv").config();
-require("any-promise/register/bluebird");
 const fs = require("fs");
 const Promise = require("bluebird");
 const path = require("path");
@@ -8,8 +7,7 @@ const _ = require("lodash");
 const moment = require("moment");
 const ActiveRecord = require("active-record");
 const bodyParser = require("body-parser");
-const request = require("request");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 fetch.Promise = Promise;
 
 const express = require("express");
@@ -97,72 +95,81 @@ router.post("/:location", restrict.toAuthor, function(req, res, next){
 }, function(req, res, next){
 	if(req.headers["content-type"] === "application/json"){
 		// Upload with URL
-		fetch(req.body.url)
-			.then((response) => {
+		Files.findBy({uid: req.params.location}).then((file) => {
+			// Do checks on the database entry
+			if(file.data === null){
+				return Promise.reject(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
+			}else if(moment(file.data.uploadExpire).isBefore(moment())){
+				file.destroy();
+				return Promise.reject(new CharError("Upload Link Expired", "This upload link has expired", 400));
+			}else if(file.data.saved_path !== null){
+				// File already exist
+				return Promise.reject(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
+			}else{
+				// All tests passed
+				return Promise.resolve(file);
+			}
+		}).then((file) => {
+			return fetch(req.body.url).then((response) => {
+				// Checks against fetched data
 				const contentType = response.headers.get("content-type");
-				if(!_.includes(limits.acceptedMIME, contentType)){
-					next(new CharError("Invalid MIME type", `File type "${contentType}" is not supported`, 415));
-				}else{
-					return Files.findBy({uid: req.params.location}).then((file) => {
-						if(file.data === null){
-							next(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
-						}else if(file.data["content-type"] !== contentType){
-							next(new CharError("MIME Type Mismatch", `File type "${response.headers["content-type"]}" does not match metadata entry`, 400));
-						}else if(moment(file.data.uploadExpire).isBefore(moment())){
-							file.destroy();
-							next(new CharError("Upload Link Expired", "This upload link has expired", 400));
-						}else if(file.data.saved_path !== null){
-							// File already exist
-							next(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
-						}else{
-							// All tests passed
-							return Promise.all([response.buffer(), file]);
-						}
-					});
+				if(!response.ok){
+					return Promise.reject(new CharError("Non-OK response code returned", `Request for resource at ${req.body.url} returned a non-OK response code.`, 400));
+				}else if(file.data["content-type"] !== contentType){
+					return Promise.reject(new CharError("MIME Type Mismatch", `File type "${contentType}" does not match metadata entry`, 400));
 				}
-			}).then((data) => {
-				const buf = data[0];
-				const file = data[1];
+				return Promise.all([response.buffer(), file]);
+			});
+		}).then((data) => {
+			const buf = data[0];
+			const file = data[1];
 
-				// Get file extension
-				const fileExt = path.extname(file.data.file_name) || "";
-				const savedName = `${file.data.uid}${fileExt}`;
-				delete file.data.uploadExpire;
-				delete file.data.uploadLocation;
-				file.data.file_size = buf.length;
-				file.data.modified_at = moment().format();
-				file.data.saved_path = path.join("./uploads/", savedName);
+			// Check length of data is within bounds
+			if(buf.length > limits.fileSize){
+				return Promise.reject(new CharError("File size too big", "The uploaded file is over the size limit acceptable.", 400));
+			}
 
-				fs.writeFile(file.data.saved_path, buf, (err) => {
-					if(err) return next(err);
+			// Get file extension
+			const fileExt = path.extname(file.data.file_name) || "";
+			const savedName = `${file.data.uid}${fileExt}`;
+			delete file.data.uploadExpire;
+			delete file.data.uploadLocation;
+			file.data.file_size = buf.length;
+			file.data.modified_at = moment().format();
+			file.data.saved_path = path.join("./uploads/", savedName);
 
-					file.save().then(() => {
-						res.json({
-							resource_path: file.data.file_permalink
-						});
-					}).catch((err) => {
-						next(err);
+			fs.writeFile(file.data.saved_path, buf, (err) => {
+				if(err) return Promise.reject(err);
+
+				file.save().then(() => {
+					res.json({
+						resource_path: file.data.file_permalink
 					});
+				}).catch((err) => {
+					next(err);
 				});
 			});
+		}).catch((err) => {
+			next(err);
+		});
 
 	}else{
 		// Upload raw image
 		if(!_.includes(limits.acceptedMIME, req.headers["content-type"])){
-			return next(new CharError("Invalid MIME type", `File type "${req.headers["content-type"]}" is not supported`, 415));
+			return Promise.reject(new CharError("Invalid MIME type", `File type "${req.headers["content-type"]}" is not supported`, 415));
 		}
 
-		Files.findBy({uid: req.params.location}).then((file) => {
+		return Files.findBy({uid: req.params.location}).then((file) => {
 			if(file.data === null){
-				next(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
+				return Promise.reject(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
 			}else if(file.data["content-type"] !== req.headers["content-type"]){
-				next(new CharError("MIME Type Mismatch", `File type "${req.headers["content-type"]}" does not match metadata entry`, 400));
+				return Promise.reject(new CharError("MIME Type Mismatch", `File type "${req.headers["content-type"]}" does not match metadata entry`, 400));
 			}else if(moment(file.data.uploadExpire).isBefore(moment())){
 				file.destroy();
-				next(new CharError("Upload Link Expired", "This upload link has expired", 400));
+				return Promise.reject(new CharError("Upload Link Expired", "This upload link has expired", 400));
 			}else if(file.data.saved_path !== null){
 				// File already exist
-				next(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
+				return Promise.reject(new CharError("Invalid upload URL", "Upload URL is invalid", 400));
 
 			}else{
 				// Get file extension
