072ba5c1bbddea5a3a8c4e13706d72a83a27173a
limzykenneth <limzy.kenneth@gmail.com>
2020-10-30 15:35:12 +0000

    Error handling for config routes

--- routes/api/config.js
+++ routes/api/config.js
@@ -16,25 +16,37 @@ router.use(restrict.toAdministrator);
 // GET routes
 // GET all configs
 router.get("/", async function(req, res, next) {
-	const configs = await Config.all();
-	res.json(configs.data);
+	try{
+		const configs = await Config.all();
+		res.json(configs.data);
+	}catch(err){
+		next(err);
+	}
 });
 
 // GET specified config
 router.get("/:config_name", async function(req, res, next) {
-	const config = await Config.findBy({config_name: req.params.config_name});
-	res.json(config.data);
+	try{
+		const config = await Config.findBy({config_name: req.params.config_name});
+		res.json(config.data);
+	}catch(err){
+		next(err);
+	}
 });
 
 // POST routes
 // POST specific config (edit only)
 router.post("/:config_name", async function(req, res, next) {
-	const config = await Config.findBy({config_name: req.params.config_name});
+	try{
+		const config = await Config.findBy({config_name: req.params.config_name});
 
-	config.data.config_value = req.body.config_value;
-	await config.save();
+		config.data.config_value = req.body.config_value;
+		await config.save();
 
-	res.json(config.data);
+		res.json(config.data);
+	}catch(err){
+		next(err);
+	}
 });
 
 module.exports = router;
